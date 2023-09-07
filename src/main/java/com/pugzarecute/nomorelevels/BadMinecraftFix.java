db0e14251a53db80e237b285644e4c814a178404
PugzAreCute <me@pugzarecute.com>
2023-09-07 20:34:23 +0530

    Util function

--- src/main/java/com/pugzarecute/nomorelevels/BadMinecraftFix.java
+++ src/main/java/com/pugzarecute/nomorelevels/BadMinecraftFix.java
@@ -1,10 +1,15 @@
 package com.pugzarecute.nomorelevels;
 
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Player;
 
 public class BadMinecraftFix {
         public static int trueXP(Player player) {
-            return (int) (getExperienceForLevel(player.experienceLevel)+ (player.experienceProgress*player.getXpNeededForNextLevel()));
+            System.out.println(player.experienceProgress);
+            System.out.println(player.getXpNeededForNextLevel());
+            return (int) (getExperienceForLevel(player.experienceLevel)+ ((double)player.experienceProgress*(double)player.getXpNeededForNextLevel()));
+
         }
 
         public static int getExperienceForLevel(int level) {
@@ -12,5 +17,8 @@ public class BadMinecraftFix {
             else if (level <= 16) return level*level+level*6;
             else if (level <= 31) return (int) (2.5*level*level-40.5*level+360);
             else return (int)(4.5 *level*level -162.5*level + 2220);
+        }
+        public static int getTrueXPCost(LocalPlayer player, int levelCost){
+            return getExperienceForLevel(player.experienceLevel) - getExperienceForLevel(player.experienceLevel-levelCost);
         }
 }

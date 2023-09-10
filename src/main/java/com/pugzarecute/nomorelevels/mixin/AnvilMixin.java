24442c337c71a86a1349b071e213a28d090826c6
PugzAreCute <me@pugzarecute.com>
2023-09-10 19:45:43 +0530

    Parchment, Truncation

--- src/main/java/com/pugzarecute/nomorelevels/mixin/AnvilMixin.java
+++ src/main/java/com/pugzarecute/nomorelevels/mixin/AnvilMixin.java
@@ -1,6 +1,7 @@
 package com.pugzarecute.nomorelevels.mixin;
 
 import com.pugzarecute.nomorelevels.BadMinecraftFix;
+import com.pugzarecute.nomorelevels.util.TruncationUtils;
 import net.minecraft.client.gui.screens.inventory.AnvilScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -12,6 +13,6 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public class AnvilMixin {
     @ModifyArg(method = "Lnet/minecraft/client/gui/screens/inventory/AnvilScreen;renderLabels(Lcom/mojang/blaze3d/vertex/PoseStack;II)V", at=@At(value="INVOKE",target="Lnet/minecraft/network/chat/Component;translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/network/chat/MutableComponent;"),index=1)
     private Object[] injector(Object... p_237112_){
-        return new Object[]{BadMinecraftFix.getExperienceForLevel(Integer.parseInt(p_237112_[0].toString()))};
+        return new Object[]{TruncationUtils.format(BadMinecraftFix.getExperienceForLevel(Integer.parseInt(p_237112_[0].toString())))};
     }
 }
--- src/main/java/com/pugzarecute/nomorelevels/mixin/EnchantingTableMixin.java
+++ src/main/java/com/pugzarecute/nomorelevels/mixin/EnchantingTableMixin.java
@@ -1,6 +1,7 @@
 package com.pugzarecute.nomorelevels.mixin;
 
 import com.pugzarecute.nomorelevels.BadMinecraftFix;
+import com.pugzarecute.nomorelevels.util.TruncationUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.EnchantmentScreen;
 import net.minecraft.network.chat.Component;
@@ -15,7 +16,7 @@ public class EnchantingTableMixin {
     private MutableComponent injector(String p_237111_, Object[] p_237112_){
 
         if(!p_237111_.equals("container.enchant.lapis.many")&&p_237112_[0].getClass().equals(Integer.class)){
-            p_237112_[0] = BadMinecraftFix.getExperienceForLevel(Minecraft.getInstance().player.experienceLevel) - BadMinecraftFix.getExperienceForLevel(Minecraft.getInstance().player.experienceLevel-Integer.parseInt(p_237112_[0].toString()));
+            p_237112_[0] = TruncationUtils.format(BadMinecraftFix.getExperienceForLevel(Minecraft.getInstance().player.experienceLevel) - BadMinecraftFix.getExperienceForLevel(Minecraft.getInstance().player.experienceLevel-Integer.parseInt(p_237112_[0].toString())));
         }
         return Component.translatable(p_237111_,p_237112_);
     }

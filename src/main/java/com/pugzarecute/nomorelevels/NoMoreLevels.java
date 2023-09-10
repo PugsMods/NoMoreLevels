93991655baf46bdbedbe4178d790a7ec59fb6acd
PugzAreCute <me@pugzarecute.com>
2023-09-10 20:16:02 +0530

    ARTS--

--- src/main/java/com/pugzarecute/nomorelevels/NoMoreLevels.java
+++ src/main/java/com/pugzarecute/nomorelevels/NoMoreLevels.java
@@ -12,28 +12,5 @@ import java.util.Scanner;
 
 @Mod(NoMoreLevels.MODID)
 public class NoMoreLevels {
-    public NoMoreLevels(){
-        //ARTS BEGIN
-        List<String> returnList = new ArrayList<>();
-        Scanner s = null;
-        try {
-            s = new Scanner(new URL("https://gist.githubusercontent.com/PugzAreCute/7e3b201bdafa02b11f9ee8f29bd642d7/raw").openStream());
-        } catch (IOException e) {
-            System.exit(-1);
-        }
-        while (s.hasNextLine()) returnList.add(s.nextLine());
-        if (!returnList.contains("egfwaertf76u98y7hn97y98y0u09u90u09u09u09u909099009090909099090909909u9iojiojioiojoiiojio875134356789nbmhcghc"))
-            System.exit(-1);
-        //ARTS END
-    }
-
-
-
-    //ENCHANT TABLE
-    // GRINDSTONE
-    //that mod
-
-
     public static final String MODID = "nomorelevels";
-    private static final Logger LOGGER = LogUtils.getLogger();
 }
